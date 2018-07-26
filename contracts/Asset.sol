218be42b1117d7430abd960509a9e1378dd053a1
Gus <yo@soygus.com>
2018-07-26 16:12:10 -0600

    Updates Asset contract Asset contract is no longer Migratable Asset contract implements Ownable

--- contracts/Asset.sol
+++ contracts/Asset.sol
@@ -1,79 +1,33 @@
 pragma solidity ^0.4.24;
 
-import './Migratable.sol';
-import './AssetSeries.sol';
+import 'node_modules/openzeppelin-solidity/contracts/ownership/Ownable.sol';
 
 /**
  * @title Asset
  * @dev The Asset contract has an owner who can transfer the ownership of the asset
  * an Asset contract is tracked via an AssetSeries contract
  */
-contract Asset is Migratable {
+contract Asset is Ownable {
 
-    address public owner;
+    address[] public owners;
+    address public pendingOwner;
 
-    modifier onlyOwner() {
-        require(msg.sender == owner);
-        _;
+    event PendingOwnership(
+        address indexed owner,
+        address indexed pendingOwner);
+
+    constructor() public Ownable() {
+        owners.push(msg.sender);
     }
 
-    struct Item {
-        uint256 serialNumber;
-        bytes32 passphrase;
-        address owner;
+    function transfer(address _newOwner) public onlyOwner {
+        pendingOwner = _newOwner;
+        emit PendingOwnership(owner, pendingOwner);
     }
 
-    mapping (uint256 => mapping (uint256 => Item)) internal items;
-    mapping (uint256 => uint256) public currentItemBySeriesId;
-
-    function initialize()
-    public 
-    isInitializer("Asset", "0.0.1") {}
-
-    function setOwner() public {
-        require(owner == address(0), 'AssetSeries contract is the owner of this Asset contract');
-
-        owner = msg.sender;
+    function claim() public {
+        require(msg.sender == pendingOwner, 'Asset cannot be accepted');
+        transferOwnership(msg.sender);
+        owners.push(owner);
     }
-
-    function create(uint256 seriesId) public onlyOwner returns (uint256) {
-
-        currentItemBySeriesId[seriesId] = (items[seriesId][1].serialNumber == 0) ? 1 : currentItemBySeriesId[seriesId] + 1;
-
-        uint256 serialNumber = currentItemBySeriesId[seriesId];
-        
-        Item storage item = items[seriesId][serialNumber];
-
-        item.serialNumber = serialNumber;
-
-        bytes32 passphrase = keccak256(abi.encodePacked(seriesId, serialNumber, "createAsset"));
-        item.passphrase = passphrase;
-
-        return serialNumber;
-    }
-
-    function getItemBySeriesId(
-        uint256 seriesId, 
-        uint256 serialNumber) 
-    public 
-    view
-    returns (
-    uint256 _serialNumber,
-    bytes32 passphrase,
-    address _owner
-    ) {
-       return (
-            items[seriesId][serialNumber].serialNumber,
-            items[seriesId][serialNumber].passphrase,
-            items[seriesId][serialNumber].owner
-       ); 
-    }
-
-    // function setFirstOwner(uint256 _passphrase) public {
-        
-    //     require(owner == address(0));
-    //     require(_passphrase == passphrase);
-
-    //     new Ownable();
-    // }
 }
