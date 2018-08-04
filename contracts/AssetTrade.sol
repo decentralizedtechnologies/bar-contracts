e0de2a7f897bca51f932df144002827279f284bc
Gus <yo@soygus.com>
2018-08-03 23:52:30 -0600

    Adds AssetTrade methods documentation

--- contracts/AssetTrade.sol
+++ contracts/AssetTrade.sol
@@ -4,9 +4,9 @@ import 'node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol';
 import './Asset.sol';
 
 /**
- * @title Asset
- * @dev The Asset contract has an owner who can transfer the ownership of the asset
- * an Asset contract is tracked via an AssetSeries contract
+ * @title AssetTrade
+ * @dev The AssetTrade contract takes an Asset and transfers it to a new owner in exchange for ETH
+ * The AssetTrade contract must be approved to transfer the Asset after deployment
  */
 contract AssetTrade {
 
@@ -40,26 +40,35 @@ contract AssetTrade {
     event ProcessedRemainder(uint256 remainder);
     event AssetPurchased(address indexed buyer, uint256 price);
 
+    /**
+    * @dev The AssetTrade constructor sets the Asset to be traded
+    * After initialization, the owner MUST approve this contract to 
+    * transfer the Asset ownership
+    */
     constructor(Asset _asset) public onlyOwner {
         asset = _asset;
     }
 
     /*
-    * @dev
+    * @dev fallback function, processes a purchase
     */
     function() public payable {
         buy();
     }
 
     /*
-    * @dev
+    * @dev Pause the sale temporarily. Call sell or sellTo to re-enable the sale
     */
     function pause() public onlyOwner {
         state = State.Paused;
     }
 
     /*
-    * @dev
+    * @dev Sets a buyer address
+    * This method works together with buyFrom
+    * If the wallet has the buyer address, then call buyFrom
+    * @param _price The price in WEI
+    * @param _buyer The buyer address
     */
     function sellTo(uint256 _price, address _buyer) public onlyOwner {
         require(_price > 0);
@@ -72,7 +81,8 @@ contract AssetTrade {
     }
 
     /*
-    * @dev
+    * @dev Everyone can purchase the Asset
+    * @param _price The Asset price in WEI
     */
     function sell(uint256 _price) public onlyOwner {
         require(_price > 0);
@@ -84,7 +94,9 @@ contract AssetTrade {
     }
 
     /*
-    * @dev
+    * @dev If the wallet matches the buyer address set by sellTo
+    * this method processes the purchase for that buyer
+    * When the sale is done, the buyer still needs to claim the Asset
     */
     function buyFrom() canBuy public payable {
         require(state == State.SellingTo);
@@ -93,7 +105,8 @@ contract AssetTrade {
     }
 
     /*
-    * @dev
+    * @dev Processes the purchase for any buyer if a price has been set
+    * by the current owner
     */
     function buy() canBuy public payable {
         require(state == State.OpenSale);
