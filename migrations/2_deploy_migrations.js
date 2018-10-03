de435a5635c2d7b276a032c267b62f8fd6bc50ba
Gus <yo@soygus.com>
2018-10-02 23:00:27 -0600

    Removes AssetSeries tests Removes AssetSeriesRegistry migrations

--- migrations/2_deploy_migrations.js
+++ migrations/2_deploy_migrations.js
@@ -1,11 +1,11 @@
 var Asset = artifacts.require("../contracts/Asset.sol");
 var AssetTrade = artifacts.require("../contracts/AssetTrade.sol");
 var AssetSeries = artifacts.require("../contracts/AssetSeries.sol");
-var AssetSeriesRegistry = artifacts.require("../contracts/AssetSeriesRegistry.sol");
+var AssetRegistry = artifacts.require("../contracts/AssetRegistry.sol");
 
 module.exports = function (deployer) {
   deployer.deploy(Asset);
   deployer.deploy(AssetTrade);
   deployer.deploy(AssetSeries);
-  deployer.deploy(AssetSeriesRegistry);
+  deployer.deploy(AssetRegistry);
 }
