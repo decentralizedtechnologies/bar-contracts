61f247258a60473537ae6a07ceb66b62eca30d21
Gus <gus@simpleico.com>
2019-04-10 18:48:05 -0500

    Creates vRC20 contract

---
+++ contracts/vRC20.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.5.2;
+
+import "./Versioned.sol";
+import "./Ownable.sol";
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
+
+/**
+ * @title vRC20
+ * @dev Register whatever you own in the Ethereum blockchain
+ * blockchainassetregistry.com
+ */
+contract vRC20 is ERC20, ERC20Detailed, Versioned, Ownable {
+
+    constructor (
+        string memory name,
+        string memory symbol,
+        uint8 decimals
+    ) public ERC20Detailed (name, symbol, decimals) {}
+
+    /**
+    * @dev Add data to the _data array
+    * @param _data string data
+    */
+    function appendData(string memory _data) public onlyOwner returns (bool) {
+        return _appendData(_data);
+    }
+}
